358c4e458c2a23750a22b57d6e9d7b0295801ccb
Franco G <gfrancojf@gmail.com>
2022-08-23 00:22:12 -0400

    se elimina table duplicadas

--- database/migrations/2022_08_10_144633_create_table_tipo_fuente.php
+++ database/migrations/2022_08_10_144633_create_table_tipo_fuente.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('tipo')->comment('tipo de fuente');
             $table->softDeletes();
             $table->timestamps();
-        });
+        })
     }
 
     /**
--- database/migrations/2022_08_18_213849_create_table_tipo_manifold.php
+++ database/migrations/2022_08_18_213849_create_table_tipo_manifold.php
@@ -13,16 +13,22 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD
-        Schema::create('tipo_manifold', function (Blueprint $table) {
-            $table->id();
-            $table->string('tipo_manifold');
-=======
-        Schema::create('table_tipo_manifold', function (Blueprint $table) {
-            $table->id();
->>>>>>> master
-            $table->timestamps();
-        });
+    Schema::create('manifold', function (Blueprint $table) {
+    $table->id();
+    $table->string('nombre')->comment('nombre del manifold');
+    $table->unsignedBigInteger('id_tipo_manifold');
+    $table->unsignedBigInteger('cant_bridas');
+    $table->unsignedBigInteger('cant_monometro');
+    $table->unsignedBigInteger('cant_valvulas');
+    $table->unsignedBigInteger('cant_tuberias');
+    $table->boolean('operatividad');
+    $table->unsignedBigInteger('id_estacion_bombeo');
+    $table->foreign('id_estacion_bombeo')->references('id')->on('estacion_bombeo');
+    $table->foreign('id_tipo_manifold')->references('id')->on('tipo_manifold');
+    $table->softDeletes();
+    $table->timestamps();
+});
+
     }
 
     /**
